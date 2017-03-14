d755ad5f65bb368892b5e3774c3673837d23b52a
luhaiyang <luhy@garena.com>
2017-03-14 18:12:08 +0800

    add includes

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,14 @@
 #include <jni.h>
 #include <string>
 
-extern "C"
+extern "C"{
+#include <libavformat/avformat.h>
+}
 JNIEXPORT jstring JNICALL
 Java_com_example_luhaiyang_ffplay_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject  thiz) {
     std::string hello = "Hello from C++";
+    av_register_all();
     return env->NewStringUTF(hello.c_str());
 }
