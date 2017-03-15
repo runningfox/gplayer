2b96ae406b07458544bb47a08d8f2aa0fe136f3e
luhaiyang <luhy@garena.com>
2017-03-15 15:25:15 +0800

    fix lib so link

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,14 +1,18 @@
 #include <jni.h>
 #include <string>
 
-extern "C"{
+
+extern "C" {
+#include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
-}
+#include <libswscale/swscale.h>
+
 JNIEXPORT jstring JNICALL
 Java_com_example_luhaiyang_ffplay_MainActivity_stringFromJNI(
         JNIEnv *env,
-        jobject  thiz) {
+        jobject) {
     std::string hello = "Hello from C++";
     av_register_all();
     return env->NewStringUTF(hello.c_str());
+}
 }
